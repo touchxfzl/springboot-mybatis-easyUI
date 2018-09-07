0938666306bf82b7a8582642db1599f1075b483d
lei <1162799332@qq.com>
2018-09-07 20:01:19 +0800

    完成文件上传案例

---
+++ src/main/java/com/easyui/controller/UploadController.java
@@ -0,0 +1,33 @@
+package com.easyui.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.easyui.utils.FileUtil;
+
+@RestController
+public class UploadController {
+	
+	@PostMapping("/upload")
+	public String upload(@RequestParam("file") MultipartFile file,
+            HttpServletRequest request){
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        /*System.out.println("fileName-->" + fileName);
+        System.out.println("getContentType-->" + contentType);*/
+        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "uploadimg success";
+		
+	}
+
+}
